ad95a2db3a82230290a8e3ab0053de4de435d2b7
flamerten <flamerten@gmail.com>
2024-03-24 01:11:22 +0800

    Health/Status bar display and animations

--- sources_1/new/Graphics/Images/KO_Text.v
+++ sources_1/new/Graphics/Images/KO_Text.v
@@ -8,47 +8,47 @@ module KO_Text(
     always @(pixel_index)
     begin
         case(pixel_index)
-        330: oled_colour = COLOUR_WHITE;
-        333: oled_colour = COLOUR_WHITE;
+        331: oled_colour = COLOUR_WHITE;
         334: oled_colour = COLOUR_WHITE;
+        335: oled_colour = COLOUR_WHITE;
+        337: oled_colour = COLOUR_WHITE;
         338: oled_colour = COLOUR_WHITE;
         339: oled_colour = COLOUR_WHITE;
         340: oled_colour = COLOUR_WHITE;
         341: oled_colour = COLOUR_WHITE;
-        342: oled_colour = COLOUR_WHITE;
 
-        426: oled_colour = COLOUR_WHITE;
-        428: oled_colour = COLOUR_WHITE;
+        427: oled_colour = COLOUR_WHITE;
         429: oled_colour = COLOUR_WHITE;
-        434: oled_colour = COLOUR_WHITE;
-        438: oled_colour = COLOUR_WHITE;
+        430: oled_colour = COLOUR_WHITE;
+        433: oled_colour = COLOUR_WHITE;
+        437: oled_colour = COLOUR_WHITE;
 
-        522: oled_colour = COLOUR_WHITE;
         523: oled_colour = COLOUR_WHITE;
         524: oled_colour = COLOUR_WHITE;
-        530: oled_colour = COLOUR_WHITE;
-        534: oled_colour = COLOUR_WHITE;
+        525: oled_colour = COLOUR_WHITE;
+        529: oled_colour = COLOUR_WHITE;
+        533: oled_colour = COLOUR_WHITE;
 
-        618: oled_colour = COLOUR_WHITE;
         619: oled_colour = COLOUR_WHITE;
         620: oled_colour = COLOUR_WHITE;
-        626: oled_colour = COLOUR_WHITE;
-        630: oled_colour = COLOUR_WHITE;   
+        621: oled_colour = COLOUR_WHITE;
+        625: oled_colour = COLOUR_WHITE;
+        629: oled_colour = COLOUR_WHITE;   
 
-        714: oled_colour = COLOUR_WHITE;
-        716: oled_colour = COLOUR_WHITE;
+        715: oled_colour = COLOUR_WHITE;
         717: oled_colour = COLOUR_WHITE;
-        722: oled_colour = COLOUR_WHITE;
-        726: oled_colour = COLOUR_WHITE;   
+        718: oled_colour = COLOUR_WHITE;
+        721: oled_colour = COLOUR_WHITE;
+        725: oled_colour = COLOUR_WHITE;   
 
-        810: oled_colour = COLOUR_WHITE;
-        813: oled_colour = COLOUR_WHITE;
+        811: oled_colour = COLOUR_WHITE;
         814: oled_colour = COLOUR_WHITE;
-        818: oled_colour = COLOUR_WHITE;
-        819: oled_colour = COLOUR_WHITE;   
+        815: oled_colour = COLOUR_WHITE;
+        817: oled_colour = COLOUR_WHITE;
+        818: oled_colour = COLOUR_WHITE;   
+        819: oled_colour = COLOUR_WHITE;
         820: oled_colour = COLOUR_WHITE;
         821: oled_colour = COLOUR_WHITE;
-        822: oled_colour = COLOUR_WHITE;
 
         default: oled_colour = 16'b00000_000000_00000;     
 
--- sources_1/new/Graphics/status_bar_update.v
+++ sources_1/new/Graphics/status_bar_update.v
@@ -4,86 +4,53 @@ module status_bar_update(
     input [4:0] curr_health_r,
 
     input [12:0] pixel_index,
-    output reg [15:0] oled_colour 
+    output reg [15:0] oled_colour,
+    
+    output [4:0] final_health_l,
+    output [4:0] final_health_r 
 );  
 
     reg [15:0] COLOUR_WHITE  = 16'hFFFF;
     reg [15:0] COLOUR_YELLOW = 16'b11111_111111_00000;
     reg [15:0] COLOUR_RED    = 16'b11111_000000_00000;
     reg [15:0] COLOUR_BLUE   = 16'b00000_000000_11111;
-    
-    //Set full health to be max of [4:0] = 31
-    parameter full_health = 31;
-    reg [4:0] prev_health_l = full_health;
-    reg [4:0] prev_health_r = full_health;
 
     wire CLOCK_ANIMATE; //1Hz                    
     CustomClock clk10hz(.CLOCK_IN(clk),
                         .COUNT_STOP(32'd50_000_000 - 1),
                         .CLOCK_OUT(CLOCK_ANIMATE));
 
-    //Animate the health bar decreasing
-    always@(posedge CLOCK_ANIMATE)
-    begin
-        if(prev_health_l > curr_health_l)
-            prev_health_l = prev_health_l - 1;
-        
-        if(prev_health_r > curr_health_r)
-            prev_health_r = prev_health_r - 1;
-    end
+    wire [15:0] hb_right_col;
+    health_bar hb_r(.health_drop_clk(CLOCK_ANIMATE),
+                    .curr_health(curr_health_r),
+                    .pixel_index(pixel_index),
+                    .oled_colour(hb_right_col),
+                    .prev_health(final_health_r));
 
 
-    wire [15:0] KO_Text;
-    KO_Text kotext(.pixel_index(pixel_index),.oled_colour(KO_Text));
-
-    integer X_HEALTH_START = 55;
-    integer Y_HEALTH_BORDER_START = 2;
-    integer Y_HEALTH_ACT_START = 3;
-    integer FULL_HEALTH_HEIGHT = 8;
-    integer FULL_HEALTH_LEN = 40;
-    integer AC_HEALTH_HEIGHT = 6;
-    
-    
     reg [12:0] mirror_pixel_index;
     always @(pixel_index)
     begin 
-        mirror_pixel_index = pixel_index - 2*(pixel_index%96 - 48);
+        mirror_pixel_index = pixel_index - 2*((pixel_index%96) - 48);
     end
 
-    /*
-    wire full_health_l; // Need to mirror this (use translated_pixel_index)
-    Draw_Rect fhl(.pixel_index(mirror_pixel_index),
-                  .X_coord_start(X_HEALTH_START),.Y_coord_start(Y_HEALTH_BORDER_START),
-                  .length(FULL_HEALTH_LEN),.height(FULL_HEALTH_HEIGHT),.draw(full_health_l));
-    */
-
-    wire draw_full_health_r;
-    Draw_Rect fhr(.pixel_index(pixel_index),
-                  .X_coord_start(X_HEALTH_START),.Y_coord_start(Y_HEALTH_BORDER_START),
-                  .length(FULL_HEALTH_LEN),.height(FULL_HEALTH_HEIGHT),.draw(draw_full_health_r));
+    wire [15:0] hb_left_col;
+    health_bar hb_l(.health_drop_clk(CLOCK_ANIMATE),
+                    .curr_health(curr_health_l),
+                    .pixel_index(mirror_pixel_index),
+                    .oled_colour(hb_left_col),
+                    .prev_health(final_health_l));    
 
 
-    wire [7:0] health_length_l;
-    assign health_length_l = (curr_health_l/full_health) * FULL_HEALTH_LEN;
-    wire [7:0] health_length_r;
-    assign health_length_r = (prev_health_r * FULL_HEALTH_LEN / full_health);
-
-    /*
-    wire curr_health_length_l; // Need to mirror this (use translated_pixel_index)
-    Draw_Rect ahl(.pixel_index(mirror_pixel_index),
-                  .X_coord_start(X_HEALTH_START),.Y_coord_start(Y_HEALTH_ACT_START),
-                  .length(health_length_l),.height(AC_HEALTH_HEIGHT),.draw(curr_health_l));*/
-
-    wire draw_curr_health_r;
-    Draw_Rect ahr(.pixel_index(pixel_index),
-                  .X_coord_start(X_HEALTH_START),.Y_coord_start(Y_HEALTH_ACT_START),
-                  .length(health_length_r),.height(AC_HEALTH_HEIGHT),.draw(draw_curr_health_r));
-
-
+    //KO Stuff ------------------------------------------------------
     wire KO_Background;
     Draw_Rect kob(.pixel_index(pixel_index),
                   .X_coord_start(42),.Y_coord_start(2),
-                  .length(12),.height(7),.draw(KO_Background)); 
+                  .length(12),.height(8),.draw(KO_Background)); 
+
+    wire [15:0] KO_Text;
+    KO_Text kotext(.pixel_index(pixel_index),.oled_colour(KO_Text));
+    //----------------------------------------------------------------
 
 
     always@(pixel_index)
@@ -91,11 +58,11 @@ module status_bar_update(
         if(KO_Text != 16'h0000)
             oled_colour = KO_Text;
         else if(KO_Background)
-            oled_colour = COLOUR_BLUE; //Debugging
-        else if (draw_curr_health_r)
-            oled_colour = COLOUR_YELLOW;            
-        else if (draw_full_health_r)
-            oled_colour = COLOUR_RED;
+            oled_colour = COLOUR_RED; //Debugging
+        else if(hb_right_col != 16'h0000)
+            oled_colour = hb_right_col;
+        else if(hb_left_col  != 16'h0000)
+            oled_colour = hb_left_col;
         else
             oled_colour = 16'h0000;
     end                               

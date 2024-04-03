29a27b9fdbb12825f5df9f6b9483326117fac9ba
Yee Kian <53704991+yeekian@users.noreply.github.com>
2024-04-03 17:59:26 +0800

    Update slaveMasterSetter

--- sources_1/new/slaveMasterSetter.v
+++ sources_1/new/slaveMasterSetter.v
@@ -20,71 +20,38 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module slaveMasterSetter(input isMaster, input clk ,input [7:0] JA, output reg [7:0] JXADC
+module slaveMasterSetter(input isMaster, input clk ,input [7:0] JA, output [7:0] JXADC
 
     //Master inputs and outputs:
     ,input oled_clk, input cs, input sdin, input sclk, input d_cn, input resn, input vccen, input pmoden
-    ,output reg player2UpBtn, output reg player2LeftBtn, output reg player2RightBtn, 
-    output reg player2AttackBtn, output reg player2DownBtn
+    ,output player2UpBtn, output player2LeftBtn, output player2RightBtn, 
+    output player2AttackBtn, output player2DownBtn
     
     //Slave inputs and outputs:
     ,input btnU, input btnD, input btnL, input btnR, input btnC
-    , output reg slave_cs, output reg slave_sdin, output reg slave_sclk, output reg slave_d_cn, output reg slave_resn, output reg slave_vccen, output reg slave_pmoden
+    , output slave_cs, output slave_sdin, output slave_sclk, output slave_d_cn, output slave_resn, output slave_vccen, output slave_pmoden
     );
         
-    always @ (posedge clk) begin
-        if (isMaster == 1) begin
-        //Master Mode
-            //Input pmod connector JA
-            player2UpBtn     <= JA[0];
-            player2DownBtn   <= JA[1];
-            player2LeftBtn   <= JA[2];
-            player2RightBtn  <= JA[3];
-            player2AttackBtn <= JA[4];
-
-            //Output pmod connector JXADC
-            JXADC[0] <= cs;
-            JXADC[1] <= sdin;
-            JXADC[2] <= sclk;
-            JXADC[3] <= d_cn;
-            JXADC[4] <= resn;
-            JXADC[5] <= vccen;
-            JXADC[6] <= pmoden;
-            
-//            //Set other outputs as LOW (0)
-//            slave_cs    <= 0;
-//            slave_sdin  <= 0;
-//            slave_sclk  <= 0;
-//            slave_d_cn  <= 0;
-//            slave_resn  <= 0; 
-//            slave_vccen <= 0;
-//            slave_pmoden <= 0;
-        end
-        
-        else begin
-        //Slave Mode
-            //Input pmod connector JA
-            slave_cs    <= JA[0];
-            slave_sdin  <= JA[1];
-            slave_sclk  <= JA[2];
-            slave_d_cn  <= JA[3];
-            slave_resn  <= JA[4]; 
-            slave_vccen <= JA[5];
-            slave_pmoden <= JA[6];
-
-            //Output pmod connector JXADC
-            JXADC[0]  <= btnU;
-            JXADC[1]  <= btnD;
-            JXADC[2]  <= btnL;
-            JXADC[3]  <= btnR;
-            JXADC[4] <= btnC;
-            
-            //Set other outputs as LOW (0)
-            player2UpBtn     <= 0;
-            player2DownBtn   <= 0;
-            player2LeftBtn   <= 0;
-            player2RightBtn  <= 0;
-            player2AttackBtn <= 0;            
-        end
-    end
+    assign JXADC[0] = isMaster ? cs     : btnU;
+    assign JXADC[1] = isMaster ? sdin   : btnD;
+    assign JXADC[2] = isMaster ? sclk   : btnL;
+    assign JXADC[3] = isMaster ? d_cn   : btnR;
+    assign JXADC[4] = isMaster ? resn   : btnC;
+    assign JXADC[5] = isMaster ? vccen  : 0;
+    assign JXADC[6] = isMaster ? pmoden : 0;
+    
+    assign player2UpBtn     = isMaster ? JA[0] :0 ;
+    assign player2DownBtn   = isMaster ? JA[1] :0 ;
+    assign player2LeftBtn   = isMaster ? JA[2] :0 ;
+    assign player2RightBtn  = isMaster ? JA[3] :0 ;
+    assign player2AttackBtn = isMaster ? JA[4] :0 ;
+    
+    assign slave_cs     = ~isMaster ? JA[0] : 0;
+    assign slave_sdin   = ~isMaster ? JA[1] : 0;
+    assign slave_sclk   = ~isMaster ? JA[2] : 0;
+    assign slave_d_cn   = ~isMaster ? JA[3] : 0;
+    assign slave_resn   = ~isMaster ? JA[4] : 0;
+    assign slave_vccen  = ~isMaster ? JA[5] : 0;
+    assign slave_pmoden = ~isMaster ? JA[6] : 0;
+    
 endmodule
